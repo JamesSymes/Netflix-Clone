b18e69ce390da8946bd88a67a1dc3629ab079dea
James Symes <jjmsymes@gmail.com>
2024-01-02 15:55:12 +0000

    update Route path

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,9 @@ const App = () => {
         <div className="app">
           <Routes>
           {/* <Route path="/" element={<HomePage />} /> */}
-            <Route path="/netflix-clone" element={<HomePage />} />
-            <Route path="/netflix-clone/about" element={<AboutPage />} /> 
-            <Route path="/netflix-clone/contact" element={<ContactPage />} />
+            <Route path="/Netflix-Clone-React" element={<HomePage />} />
+            <Route path="/Netflix-Clone-React/about" element={<AboutPage />} /> 
+            <Route path="/Netflix-Clone-React/contact" element={<ContactPage />} />
           </Routes>
         </div>
         <MobileNav />
